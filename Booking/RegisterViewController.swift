201030514b597c04d425979186a72635ca2a5093
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-10 18:06:28 +0530

    Update RegisterViewController.swift

--- Booking/RegisterViewController.swift
+++ Booking/RegisterViewController.swift
@@ -41,51 +41,59 @@ class RegisterViewController: UIViewController {
             if error != nil {
                 print("hello:",error!)
             } else {
-                for document in (snapshot?.documents)! {
-                    
-                    
-                    if let id = document.data()["id"] as? String {
-                        
-                        
-                        
-                        let typedId = self.uniqueIDTextField.text!
-                        
-                        if id == typedId{
+                db.collection("Users").getDocuments { (snapshot, error)in
+                    if error != nil{
+                        print("secondLevel error:",error!)
+                    }else {
+                        for document in (snapshot?.documents)! {
                             
-                            Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
-                                if error != nil {
-                                    print(error!)
-                                }else {
-                                    // Add a new document in collection "cities"
-                                    db.collection("Users").document(c.name).setData([
-                                        "name": c.name,
-                                        "email": c.email,
-                                        "uniqueId": c.id
-                                    ]) { err in
-                                        if let err = err {
-                                            print("Error writing document: \(err)")
-                                        } else {
-                                            print("Document successfully written!")
+                            
+                            if let id = document.data()["id"] as? String, let uid = document.data()["uniqueId"] as? String {
+                                
+                                
+                                
+                                let typedId = self.uniqueIDTextField.text!
+                                
+                                if id == typedId && typedId != uid{
+                                    
+                                    Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                                        if error != nil {
+                                            print(error!)
+                                        }else {
+                                            // Add a new document in collection "cities"
+                                            db.collection("Users").document(c.name).setData([
+                                                "name": c.name,
+                                                "email": c.email,
+                                                "uniqueId": c.id
+                                            ]) { err in
+                                                if let err = err {
+                                                    print("Error writing document: \(err)")
+                                                } else {
+                                                    print("Document successfully written!")
+                                                }
+                                            }
+                                            
+                                            //                                    mail = self.emailTextField.text!
+                                            print("Registration success")
+                                            
+                                            
+                                            //SVProgressHUD.dismiss()
+                                            
+                                            self.performSegue(withIdentifier: "goToBooking", sender: self)
+                                            
+                                
                                         }
                                     }
                                     
-                                    //                                    mail = self.emailTextField.text!
-                                    print("Registration success")
-                                    
-                                    
-                                    //SVProgressHUD.dismiss()
-                                    
-                                    self.performSegue(withIdentifier: "goToBooking", sender: self)
-                                    
-                        
                                 }
+                                
                             }
                             
                         }
                         
                     }
-                    
                 }
+                
                 
             }
         }
