a56c0e96611e18598d2aa35f78d4b029bdda72c6
anuragkurosaki <60319638+anuragkurosaki@users.noreply.github.com>
2020-05-10 18:40:42 +0530

    Update RegisterViewController.swift

--- Booking/RegisterViewController.swift
+++ Booking/RegisterViewController.swift
@@ -37,67 +37,111 @@ class RegisterViewController: UIViewController {
 //        retrieveData()
         let c = User.init(email: emailTextField.text!, name: usernameTextField.text!, id: uniqueIDTextField.text!)
         let db = Firestore.firestore()
-        db.collection("Uid").getDocuments { (snapshot, error) in
-            if error != nil {
-                print("hello:",error!)
-            } else {
-                db.collection("Users").getDocuments { (snapshot, error)in
-                    if error != nil{
-                        print("secondLevel error:",error!)
-                    }else {
-                        for document in (snapshot?.documents)! {
-                            
-                            
-                            if let id = document.data()["id"] as? String, let uid = document.data()["uniqueId"] as? String {
-                                
-                                
-                                
-                                let typedId = self.uniqueIDTextField.text!
-                                
-                                if id == typedId && typedId != uid{
-                                    
-                                    Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
-                                        if error != nil {
-                                            print(error!)
-                                        }else {
-                                            // Add a new document in collection "cities"
-                                            db.collection("Users").document(c.name).setData([
-                                                "name": c.name,
-                                                "email": c.email,
-                                                "uniqueId": c.id
-                                            ]) { err in
-                                                if let err = err {
-                                                    print("Error writing document: \(err)")
-                                                } else {
-                                                    print("Document successfully written!")
-                                                }
+        db.collection("Uid").whereField("id", isEqualTo: c.id)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    db.collection("Users").whereField("uniqueId", isEqualTo: c.id)
+                        .getDocuments() { (querySnapshot, err) in
+                            if let err = err {
+                                print("Error getting documents: \(err)")
+                            } else {
+                                Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+                                    if error != nil {
+                                        print(error!)
+                                    }else {
+                                        // Add a new document in collection "cities"
+                                        db.collection("Users").document(c.name).setData([
+                                            "name": c.name,
+                                            "email": c.email,
+                                            "uniqueId": c.id
+                                        ]) { err in
+                                            if let err = err {
+                                                print("Error writing document: \(err)")
+                                            } else {
+                                                print("Document successfully written!")
                                             }
-                                            
-                                            //                                    mail = self.emailTextField.text!
-                                            print("Registration success")
-                                            
-                                            
-                                            //SVProgressHUD.dismiss()
-                                            
-                                            self.performSegue(withIdentifier: "goToBooking", sender: self)
-                                            
-                                
                                         }
+
+                                        //                                    mail = self.emailTextField.text!
+                                        print("Registration success")
+
+
+                                        //SVProgressHUD.dismiss()
+
+                                        self.performSegue(withIdentifier: "goToBooking", sender: self)
+
+
                                     }
-                                    
                                 }
-                                
+                                }
                             }
-                            
-                        }
-                        
+                    }
                     }
                 }
-                
-                
-            }
         }
-    }
-}
-
+//        db.collection("Uid").getDocuments { (snapshot, error) in
+//            if error != nil {
+//                print("hello:",error!)
+//            } else {
+//                db.collection("Users").getDocuments { (snapshot, error)in
+//                    if error != nil{
+//                        print("secondLevel error:",error!)
+//                    }else {
+//                        for document in (snapshot?.documents)! {
+//
+//
+//                            if let id = document.data()["id"] as? String, let uid = document.data()["uniqueId"] as? String {
+//
+//
+//
+//                                let typedId = self.uniqueIDTextField.text!
+//
+//                                if id == typedId && typedId != uid{
+//
+//                                    Auth.auth().createUser(withEmail: self.emailTextField.text!, password: self.passwordTextField.text!) { (user, error) in
+//                                        if error != nil {
+//                                            print(error!)
+//                                        }else {
+//                                            // Add a new document in collection "cities"
+//                                            db.collection("Users").document(c.name).setData([
+//                                                "name": c.name,
+//                                                "email": c.email,
+//                                                "uniqueId": c.id
+//                                            ]) { err in
+//                                                if let err = err {
+//                                                    print("Error writing document: \(err)")
+//                                                } else {
+//                                                    print("Document successfully written!")
+//                                                }
+//                                            }
+//
+//                                            //                                    mail = self.emailTextField.text!
+//                                            print("Registration success")
+//
+//
+//                                            //SVProgressHUD.dismiss()
+//
+//                                            self.performSegue(withIdentifier: "goToBooking", sender: self)
+//
+//
+//                                        }
+//                                    }
+//
+//                                }
+//
+//                            }
+//
+//                        }
+//
+//                    }
+//                }
+//
+//
+//            }
+//        }
+//    }
+//}
+//
 
